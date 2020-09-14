d39cc3c66dc4485444e929f0759ef18906f8b487
Hai Ha Nguyen <haicorp87@gmai.com>
2020-09-15 00:41:52 +0200

    End of Section 5 - 61 - adding Form component, selecting an individual Activity

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -8,6 +8,9 @@ import ActivityDashboard from "../../features/activities/dashboard/ActivityDashb
 
 const App = () => {
   const [activities, setActivities] = useState<IActivity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(
+    null
+  );
 
   useEffect(() => {
     axios
@@ -17,11 +20,22 @@ const App = () => {
       });
   }, []);
 
+  const handleSelectActivity = (id: string) => {
+    debugger;
+    setSelectedActivity(activities.filter((a) => a.id === id)[0]);
+  };
+
+  console.log("activities", activities);
+
   return (
     <>
       <NavBar />
       <Container style={{ marginTop: "7rem" }}>
-        <ActivityDashboard activities={activities} />
+        <ActivityDashboard
+          activities={activities}
+          selectActivity={handleSelectActivity}
+          selectedActivity={selectedActivity}
+        />
       </Container>
     </>
   );
